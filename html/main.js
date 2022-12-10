c54faf27a05ed7e89fa4ae3d52868f80c78383e7
Horizon35m <92321696+Horizon35m@users.noreply.github.com>
2022-12-10 06:55:02 -0800

    all functionallity in but needs pollish

--- html/main.js
+++ html/main.js
@@ -1,5 +1,5 @@
-var track_name = document.querySelector("songname");
-var track_artist = document.querySelector("songartist");
+var track_name = document.getElementById("changesong");
+var track_artist = document.getElementById("changeartist");
 
 const playbtn = document.getElementById("playbtn");
 const play = document.getElementById("play");
@@ -9,14 +9,14 @@ const back = document.getElementById("back");
 var slider = document.getElementById("myRange");
 
 
-var track_index = 0;
-var isPlaying = false;
+var player = document.getElementById("music"); // Get Audio Element
 
-var curr_track = document.getElementById('music');
+var lastSong = null;
+var selection = null;
 
-var track_list = [
+var playlist = [
     {
-        name: "energetic-indie-rock-jump",
+        name: "rock-jump",
         artist: "AlexGrohl",
         path: "assets/music/AlexGrohl~energetic-indie-rock-jump.mp3",
     },
@@ -26,92 +26,34 @@ var track_list = [
         path: "assets/music/qubesounds~action-protection.mp3",
     },
     {
-        name: "powerful-gym-rock.mp3",
+        name: "powerful-gym-rock",
         artist: "qubesounds",
         path: "assets/music/qubesounds~powerful-gym-rock.mp3",
     },
     {
-        name: "inspiring-motivational-rock-inspire-mesenses",
+        name: "motivational-rock",
         artist: "AlexGrohl",
         path: "assets/music/AlexGrohl~inspiring-motivational-rock-inspire-mesenses.mp3",
     },
 ];
 
 
-function loadTrack(track_index) {
+player.autoplay=true;
+player.addEventListener("ended", selectRandom); // Run function when song ends
 
-  // Load a new track
-  curr_track.src = track_list[track_index].path;
-  curr_track.load();
- 
-  // Update details of the track
-  track_name.textContent = track_list[track_index].name;
-  track_artist.textContent = track_list[track_index].artist;
-  now_playing.textContent =
-     "PLAYING " + (track_index + 1) + " OF " + track_list.length;
- 
- 
- 
-  // Move to the next track if the current finishes playing
-  // using the 'ended' event
-  curr_track.addEventListener("ended", nextTrack);
- 
-  // Apply a random background color
-  random_bg_color();
-}
- 
-// Load the first track in the tracklist
-loadTrack(track_index);
+function selectRandom(){
+    while(selection == lastSong){ // Repeat until different song is selected
+        selection = Math.floor(Math.random() * playlist.length);
+    }
+    lastSong = selection; // Remember last song
+    player.src = playlist[selection].path;
+    track_name.innerHTML = playlist[selection].name;
+    track_artist.innerHTML = playlist[selection].artist;
 
-function playPause() {
-  // Switch between playing and pausing
-  // depending on the current state
-  if (!isPlaying) playTrack();
-  else pauseTrack();
-}
- 
-function playTrack() {
-  // Play the loaded track
-  curr_track.play();
-  isPlaying = true;
- 
-  // Replace icon with the pause icon
-  playpause_btn.innerHTML = '<i class="fa fa-pause-circle fa-5x"></i>';
 }
 
-function pauseTrack() {
-  // Pause the loaded track
-  curr_track.pause();
-  isPlaying = false;
- 
-  // Replace icon with the play icon
-  playpause_btn.innerHTML = '<i class="fa fa-play-circle fa-5x"></i>';
-}
-
-function forwardTrack() {
-  // Go back to the first track if the
-  // current one is the last in the track list
-  if (track_index < track_list.length - 1)
-    track_index += 1;
-  else track_index = 0;
- 
-  // Load and play the new track
-  loadTrack(track_index);
-  playTrack();
-}
-
-function backTrack() {
-  // Go back to the last track if the
-  // current one is the first in the track list
-  if (track_index > 0)
-    track_index -= 1;
-  else track_index = track_list.length - 1;
-   
-  // Load and play the new track
-  loadTrack(track_index);
-  playTrack();
-}
-
+selectRandom(); // Select initial song
+player.play(); // Start Song
 
 
 music.volume = 0.2;
@@ -119,10 +61,24 @@ slider.oninput = function() {
     music.volume = this.value / 100;
 } 
 
+function playPause() {
+  return player.paused ? player.play() : player.pause();
+};
+
+playbtn.addEventListener('click', function() {
+    playPause();
+    console.log("play clicked")
+});
+
 back.addEventListener('click', function() {
+    player.currentTime = 0;
+    player.play();
+    console.log("started over");
 });
 
 forward.addEventListener('click', function() {
+    selectRandom();
+    player.play();
 });
 
 var count = 0;
@@ -155,5 +111,4 @@ window.addEventListener('message', function (e) {
     (handlers[e.data.eventName] || function () { })(e.data);
 });
 
-// document.getElementById("something").textContent = 'new text';
 
